56ad2cc8dbec3597e5dda97ed88c3901dd3f9c77
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-02-01 00:09:46 +0200

    Task12solved

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Movies Repo
 **Author Name**: Ibrahim Alaqoul.
 
 ## WRRC
-![image](/WRRC.JPG)
-
+![image](/WRRC.jpg)
+![image2](WRRC2.jpg)
 ## Overview
 Movie Application that  you'll find Every intresting movie in it.
 ## Getting Started
-For the first step i called the express framework , and i did declared the express as a func. in a constant called myapp. then i did the command listen to make sure it's alive.
-
-
+For the first step i called the express framework , and i did declared the express as a func. in a constant called myapp. then i did the command listen to make sure it's alive.For the endpoints i did use get command wich take two params the first is the path and the second is the function i will call to send the data .
+for today it was different like we have to get data from API , it was super easy as long as i understand how i deal with data from the API.
+leant how to decument any Online API.
 ## Project Features
-For the endpoints i did use get command wich take two params the first is the path and the second is the function i will call to send the data .
+Movies app to search for any intersting movie or tv show you look in.
--- server.js
+++ server.js
@@ -2,22 +2,93 @@
 const express = require('express');
 const myapp = express();
 const newData = require("./Movie data/data.json");
+const dotenv = require('dotenv');
+const axios = require("axios");
+dotenv.config();
+const PORT = process.env.PORT;
+const APIKEY = process.env.APIKEY;
+
 myapp.get('/',dataConstructerHandler);
 myapp.get('/favorite',dataHandler)
+myapp.get('/trending',getMoviesHandler)
+myapp.get('/searchmovie',searchForMovie)
+myapp.get('/randompath',anotherAPIfunction)
+myapp.use(errorHandler);
+
+
+   function anotherAPIfunction(req,res){
+       let newArray=[];
+    axios.get(`https://api.themoviedb.org/3/movie/changes?api_key=${APIKEY}&page=1`).then(value =>{
+    value.data.results.forEach(value =>{
+        newArray.push(value);
+    });return res.status(200).json(newArray);
+    }).catch(error => {
+        errorHandler(error, req,res);
+    });
+   }
+
+
+
+
+function searchForMovie(req, res){
+    console.log(req.query.search);
+    let searchquery = req.query.search;
+    let myArray=[];
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${searchquery}`).then(value => {
+        
+        value.data.results.forEach(movies => {
+            let movie = new DataConstructer (movies.title,movies.poster_path,movies.overview);
+            myArray.push(movie);
+        })
+
+        return res.status(200).json(myArray);
+    }).catch(error => {
+        errorHandler(error, req,res);
+    })
+
+}
+
+
+
+
+
+function errorHandler(error, req, res){
+    const err = {
+        status : 500,
+        message : error
+    }
+
+    res.status(500).send(err);
+};
 
 function dataConstructerHandler(req,res){
     let Dataone = new DataConstructer (newData.title,newData.poster_path,newData.overview);
     return res.status(200).json(Dataone);
 };
-function DataConstructer(title,poster_path,overview){
+function DataConstructer(id,title,release_date,poster_path,overview){
+    this.id=id;
     this.title =title,
+    this.release_date=release_date;
     this.poster_path = poster_path,
     this.overview =overview 
 };
 
 function dataHandler(req,res){
  return res.status(200).send('Welcome to Favorite Page');
+
 };
-myapp.listen(3000, () => {
-    console.log('Listen to port 3000');
-})
+
+function getMoviesHandler(req,res){
+    axios.get(`https://api.themoviedb.org/3/movie/634649?api_key=${APIKEY}`).then(value => {
+        console.log(value.data);
+            let movieOne = new DataConstructer (value.data.id,value.data.title,value.data.release_date,value.data.poster_path,value.data.overview);            
+         return res.status(200).json(movieOne);
+    }).catch(error => {
+        errorHandler(error, req,res);
+    
+    });
+};
+
+myapp.listen(PORT, () => {
+    console.log(`Listen to port ${PORT}`);
+});

f2d5885038d9181261644d543459aa1601d53973
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-02-01 21:29:13 +0200

    task13solved

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Movies Repo
 ## WRRC
 ![image](/WRRC.jpg)
 ![image2](WRRC2.jpg)
+![image3](WRRC3.jpg)
 
 ## Overview
 Movie Application that  you'll find Every intresting movie in it.
@@ -14,5 +15,6 @@ Movie Application that  you'll find Every intresting movie in it.
 For the first step i called the express framework , and i did declared the express as a func. in a constant called myapp. then i did the command listen to make sure it's alive.For the endpoints i did use get command wich take two params the first is the path and the second is the function i will call to send the data .
 for today it was different like we have to get data from API , it was super easy as long as i understand how i deal with data from the API.
 leant how to decument any Online API.
+Today we did post req to the database we built and did get the data from the local rever as it is the same the client.
 ## Project Features
 Movies app to search for any intersting movie or tv show you look in.
--- server.js
+++ server.js
@@ -4,19 +4,26 @@ const myapp = express();
 const newData = require("./Movie data/data.json");
 const dotenv = require('dotenv');
 const axios = require("axios");
+const pg = require("pg");
+
 dotenv.config();
 const PORT = process.env.PORT;
 const APIKEY = process.env.APIKEY;
+const DATABASE_URL = process.env.DATABASE_URL;
+const client = new pg.Client(DATABASE_URL);
+myapp.use(express.json());
 
 myapp.get('/',dataConstructerHandler);
 myapp.get('/favorite',dataHandler)
 myapp.get('/trending',getMoviesHandler)
 myapp.get('/searchmovie',searchForMovie)
 myapp.get('/randompath',anotherAPIfunction)
+myapp.post("/addFavMovies", addMoviesHandler);
+myapp.get("/getAllFavMovies", getAllFavMoviesHandler);
+
 myapp.use(errorHandler);
 
-
-   function anotherAPIfunction(req,res){
+     function anotherAPIfunction(req,res){
        let newArray=[];
     axios.get(`https://api.themoviedb.org/3/movie/changes?api_key=${APIKEY}&page=1`).then(value =>{
     value.data.results.forEach(value =>{
@@ -46,12 +53,7 @@ function searchForMovie(req, res){
         errorHandler(error, req,res);
     })
 
-}
-
-
-
-
-
+};
 function errorHandler(error, req, res){
     const err = {
         status : 500,
@@ -92,7 +94,31 @@ function getMoviesHandler(req,res){
     
     });
 };
+function addMoviesHandler(req,res){
+    console.log(req.body);
+    let movie = req.body
+    const sql = `INSERT INTO favMovies(title,release_date,poster_path,overview,comment) VALUES($1, $2, $3, $4, $5) RETURNING * ;`
+    let values = [movie.title, movie.release_date, movie.poster_path, movie.overview ,movie.comment];
+    client.query(sql, values).then((data) => {
+       
+        return res.status(201).json(data.rows);
+    }).catch(error => {
+        errorHandler(error, req, res);
+    })
+};
+function getAllFavMoviesHandler(req, res){
+    const sql = `SELECT * FROM favMovies`;
+    client.query(sql).then(data => {
+        return res.status(200).json(data.rows);
+    }).catch(error => {
+        errorHandler(error, req,res);
+    })
+}
 
-myapp.listen(PORT, () => {
-    console.log(`Listen to port ${PORT}`);
+
+
+client.connect().then(() => {
+    myapp.listen(PORT, () => {
+        console.log(`I am using port ${PORT}`);
+    });
 });
